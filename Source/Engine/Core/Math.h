1420c8389619ba399c24753368a1bd2a91654f2d
BaconEater314 <nickbeaty314@gmail.com>
2025-07-01 10:15:46 -0600

    class work 7/1/2025

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace bacon {
+	constexpr float pi = 3.14159265358979323846264338327950288419f;
+	constexpr float twoPi = 2 * pi;
+	constexpr float halfPi = pi / 2;
+}
---
+++ Source/Game/Main.cpp
@@ -0,0 +1,10 @@
+#include "Core/Math.h"
+#include <iostream>
+
+#define NAME "Nick\n"
+
+int main() {
+	std::cout << NAME;
+	std::cout << "Hello World\n";
+	std::cout << bacon::pi << std::endl;
+}
