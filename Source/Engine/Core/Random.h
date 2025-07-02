715bb1e0052317aa4fe9b3f94b0b54990e9bb158
BaconEater314 <nickbeaty314@gmail.com>
2025-07-02 10:17:17 -0600

    did class work 7/2/2025

--- Source/Engine/Core/Random.h
+++ Source/Engine/Core/Random.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <cstdlib>
+
+namespace bacon {
+	namespace random {
+		//generates a random integer value
+		inline int getRandom() { return rand(); }
+
+		//generates a random integer in the range [0, max)
+		inline int getRandom(int max) { return rand() % max; }
+
+		//generates a random integer in the range (min, max)
+		inline int getRandom(int max, int min) { return min + getRandom(max - min + 1); }
+
+		//generates a random float in the range [0.0, 1.0]
+		inline float getRandomFloat() { return rand() / (float)RAND_MAX; }
+	}
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,10 +1,22 @@
-#include "Core/Math.h"
+#include "Math/Math.h"
+#include "Core/Random.h"
 #include <iostream>
+#include <algorithm>
 
 #define NAME "Nick\n"
 
+using namespace bacon;
+
 int main() {
+	const float num = math::RadToDeg(math::pi);
+	math::min(5, 3);
+	math::clamp(1,2,3);
+
 	std::cout << NAME;
 	std::cout << "Hello World\n";
-	std::cout << bacon::pi << std::endl;
+	std::cout << math::pi << std::endl;
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << random::getRandomFloat() << std::endl;
+	}
 }
